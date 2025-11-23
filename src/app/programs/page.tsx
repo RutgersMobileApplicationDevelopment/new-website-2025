0d2266efd024142e8125fd3d9aa28cc2a63f5951
Ayush Patel <ayush.patel1@rutgers.edu>
2025-11-22 19:53:27 -0500

    add: added dependencies for three.js/react-three, started basic page routing.

---
+++ src/app/programs/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Canvas } from "@react-three/fiber";
+
+export default function ProjectPage() {
+  return (
+    <main className="relative h-screen w-full overflow-hidden bg-[#0d0d0d]">
+      
+    </main>
+  );
+}
